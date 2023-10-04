8843dd215892c927e88f1ea1506fba88f64ed4ed
cesar <cesarcasas0737@gmail.com>
2023-10-04 14:54:57 -0700

    Added idols information to sort

---
+++ react_rendering_list/src/idols.jsx
@@ -0,0 +1,64 @@
+export const idols = [{
+    id: 0,
+    stageName: 'Sana',
+    nationality: 'Japanese',
+    group: 'Twice',
+    mbti: 'ENFP',
+    height: '168',
+    weight: '47',
+    imageId: ''
+}, {
+    id: 1,
+    stageName: 'Sua',
+    nationality: 'Korean',
+    group: 'Dreamcatcher',
+    mbti: 'ESFJ',
+    height: '162',
+    weight: '42',
+    imageId: ''
+}, {
+    id: 2,
+    stageName: 'Jiu',
+    nationality: 'Korean',
+    group: 'Dreamcatcher',
+    mbti: 'ENFP',
+    height: '167',
+    weight: '49',
+    imageId: ''
+}, {
+    id: 3,
+    stageName: 'Seulgi',
+    nationality: 'Korean',
+    group: 'Red Velvet',
+    mbti: 'ISFP',
+    height: '164',
+    weight: '44',
+    imageId: ''
+}, {
+    id: 4,
+    stageName: 'Miyeon',
+    nationality: 'Korean',
+    group: '(G)-IDLE',
+    mbti: 'ENFP',
+    height: '161',
+    weight: '45',
+    imageId: ''
+}, {
+    id: 5,
+    stageName: 'Momo',
+    nationality: 'Japanese',
+    group: 'Twice',
+    mbti: 'INFP',
+    height: '167',
+    weight: '48.5',
+    imageId: ''
+}, {
+    id: 6,
+    stageName: 'Handong',
+    nationality: 'Chinese',
+    group: 'Dreamcatcher',
+    mbti: 'ISFP',
+    height: '165',
+    weight: '46',
+    imageId: ''
+}];
