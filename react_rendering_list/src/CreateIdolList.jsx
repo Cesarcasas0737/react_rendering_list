0387f493f512b8a9ef12bfa85bfaa730b779a8e5
cesar <cesarcasas0737@gmail.com>
2023-10-04 16:17:34 -0700

    deployed CreateIdolList into main

---
+++ react_rendering_list/src/CreateIdolList.jsx
@@ -0,0 +1,20 @@
+import { idols } from './idols.jsx';
+import { getImageUrl } from './utils.jsx';
+
+export default function List() {
+  const listItems = idols.map(idol =>
+    <li key={idol.id}>
+      <img
+        src={getImageUrl(idol)}
+        alt={idol.stageName}
+      />
+      <p>
+        <b>{idol.stageName}</b>
+          {' ' + idol.nationality + ' '}
+          known for {idol.mbti}
+      </p>
+    </li>
+  );
+
+  return <ul>{listItems}</ul>;
+}
